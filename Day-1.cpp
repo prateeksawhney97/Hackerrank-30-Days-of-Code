1f10305ce2e04baa4f8ae627ddecb7278fa6937c
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-18 22:18:24 +0530

    Update Day-1.cpp

--- Day-1.cpp
+++ Day-1.cpp
@@ -1,11 +1,23 @@
-int a;
-double b;
-string c, result;
-cin >> a;
-cin >> b;
-//cin >> c;
-getline(cin >> ws, c);
-double sum=b+d;
-cout << a+i << endl;
-cout << fixed << setprecision(1) << sum << endl;
-cout << s+c << endl;
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    int a;
+    double b;
+    string c, result;
+    cin >> a;
+    cin >> b;
+    //cin >> c;
+    getline(cin >> ws, c);
+    double sum=b+d;
+    cout << a+i << endl;
+    cout << fixed << setprecision(1) << sum << endl;
+    cout << s+c << endl;
+    return 0;
+}
