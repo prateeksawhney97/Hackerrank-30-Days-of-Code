ac411c1577325b58053518a81521eca1a971717f
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-18 22:17:03 +0530

    Create Day-1.cpp

---
+++ Day-1.cpp
@@ -0,0 +1,11 @@
+int a;
+double b;
+string c, result;
+cin >> a;
+cin >> b;
+//cin >> c;
+getline(cin >> ws, c);
+double sum=b+d;
+cout << a+i << endl;
+cout << fixed << setprecision(1) << sum << endl;
+cout << s+c << endl;
